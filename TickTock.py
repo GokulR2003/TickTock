4b94e49a6eed96288ee04fcd5b412b22e649e631
GokulR2003 <139532441+GokulR2003@users.noreply.github.com>
2024-06-16 16:16:05 +0530

    Update TickTock.py

--- TickTock.py
+++ TickTock.py
@@ -1,25 +1,26 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 import time
 import math
 import pygame
 pygame.mixer.init()
 tick_sound = pygame.mixer.Sound("tick.wav")
 app_window = tk.Tk()
-app_window.title("Clock")
+app_window.title("Python Clock")
 app_window.geometry("500x500")
 app_window.resizable(False, False)
 time_font = ("Helvetica", 14, 'bold')
 date_font = ("Helvetica", 10)
-background_color = "#282c34"
-circle_color = "#61dafb"
-time_color = "#ffffff"
-date_color = "#ffffff"
 hand_color = "#ffffff"
-canvas = tk.Canvas(app_window, width=500, height=500, bg=background_color, highlightthickness=0)
+bg_image = Image.open("Background.jpg")
+bg_image = bg_image.resize((500, 500), Image.ANTIALIAS)
+bg_photo = ImageTk.PhotoImage(bg_image)
+canvas = tk.Canvas(app_window, width=500, height=500)
 canvas.pack()
+canvas.create_image(0, 0, anchor=tk.NW, image=bg_photo)
 center_x, center_y = 250, 250
 radius = 200
-canvas.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, fill=circle_color, outline="#282c34", width=4)
+canvas.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, fill="", outline="#282c34", width=4)
 for i in range(12):
     angle = math.radians((i / 12) * 360)
     x_start = center_x + radius * 0.9 * math.cos(angle)
@@ -27,8 +28,8 @@ for i in range(12):
     x_end = center_x + radius * 0.8 * math.cos(angle)
     y_end = center_y + radius * 0.8 * math.sin(angle)
     canvas.create_line(x_start, y_start, x_end, y_end, fill=hand_color, width=2)
-label_time = tk.Label(app_window, font=time_font, bg=circle_color, fg=time_color)
-label_date = tk.Label(app_window, font=date_font, bg=circle_color, fg=date_color)
+label_time = tk.Label(app_window, font=time_font, bg="", fg=hand_color)
+label_date = tk.Label(app_window, font=date_font, bg="", fg=hand_color)
 canvas.create_window(center_x, center_y + 40, window=label_time)
 canvas.create_window(center_x, center_y + 60, window=label_date)
 def update_clock():
